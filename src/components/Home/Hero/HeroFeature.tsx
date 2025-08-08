93735071fab3f1c3bdeedf0007d957e74c90a3ee
João Gabriel <joaoname9@gmail.com>
2025-08-08 04:09:14 -0300

    refactor: HeroFeature

--- src/components/Home/Hero/HeroFeature.tsx
+++ src/components/Home/Hero/HeroFeature.tsx
@@ -1,37 +1,31 @@
 import React from "react";
-import Image from "next/image";
+import { Repeat2, ShieldCheck, HeadphonesIcon } from "lucide-react";
 
 const featureData = [
   {
-    img: "/images/icons/icon-01.svg",
-    title: "Free Shipping",
-    description: "For all orders $200",
+    icon: <Repeat2 size={40} />,
+    title: "Devolução em 7 dias",
+    description: "Cancelamento após 7 dias",
   },
   {
-    img: "/images/icons/icon-02.svg",
-    title: "1 & 1 Returns",
-    description: "Cancellation after 1 day",
+    icon: <ShieldCheck size={40} />,
+    title: "Pagamento 100% Seguro",
+    description: "Garantia de pagamentos seguros",
   },
   {
-    img: "/images/icons/icon-03.svg",
-    title: "100% Secure Payments",
-    description: "Gurantee secure payments",
-  },
-  {
-    img: "/images/icons/icon-04.svg",
-    title: "24/7 Dedicated Support",
-    description: "Anywhere & anytime",
+    icon: <HeadphonesIcon size={40} />,
+    title: "Suporte 24/7",
+    description: "Em qualquer lugar e hora",
   },
 ];
 
 const HeroFeature = () => {
   return (
     <div className="max-w-[1060px] w-full mx-auto px-4 sm:px-8 xl:px-0">
-      <div className="flex flex-wrap items-center gap-7.5 xl:gap-12.5 mt-10">
+      <div className="flex flex-row justify-between flex-wrap items-center gap-7.5 xl:gap-12.5 mt-10">
         {featureData.map((item, key) => (
           <div className="flex items-center gap-4" key={key}>
-            <Image src={item.img} alt="icons" width={40} height={41} />
-
+            {item.icon}
             <div>
               <h3 className="font-medium text-lg text-dark">{item.title}</h3>
               <p className="text-sm">{item.description}</p>
