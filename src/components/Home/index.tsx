345dab49dc7ab32f82b11924a0beee62e57e6da3
João Gabriel <joaoname9@gmail.com>
2025-08-09 22:27:09 -0300

    remove: useless code

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,7 +3,6 @@ import Hero from "./Hero";
 import NewArrival from "./NewArrivals";
 import PromoBanner from "./PromoBanner";
 import BestSeller from "./BestSeller";
-import CounDown from "./Countdown";
 import Testimonials from "./Testimonials";
 
 const Home = () => {
@@ -13,7 +12,6 @@ const Home = () => {
       <NewArrival />
       <PromoBanner />
       <BestSeller />
-      <CounDown />
       <Testimonials />
     </main>
   );
